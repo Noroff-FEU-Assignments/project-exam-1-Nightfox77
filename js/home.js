576f6c08630342828905ccea35a7a22497fd9ba2
Michael Bäuml <102950224+Nightfox77@users.noreply.github.com>
2023-09-24 15:06:31 +0200

    1

--- js/home.js
+++ js/home.js
@@ -37,80 +37,142 @@ closeIconSearch.onclick = function closeSearch() {
 }
 
 
-let currentIndex = 0;
-const containers = document.querySelectorAll(".blogcontainer");
-const arrowRight = document.querySelector('.arrow.right');
-const arrowLeft = document.querySelector('.arrow.left');
 
-const url = 'https://nightfox.no/JapanTravelBlog/wp-json/wc/v3/products/?per_page=20&consumer_key=ck_c69945534ca43c9d86de2416bc85941f76ad51b3&consumer_secret=cs_73f299335b404f1004241d0b181562359dd479a3';
+let currentIndex = 0;
+const arrowRight = document.querySelector(".arrow.right");
+const arrowLeft = document.querySelector(".arrow.left");
+const carouselContainer = document.querySelector(".slider");
+const url = "https://nightfox.no/JapanTravelBlog/wp-json/wc/v3/products/?per_page=20&consumer_key=ck_c69945534ca43c9d86de2416bc85941f76ad51b3&consumer_secret=cs_73f299335b404f1004241d0b181562359dd479a3";
 
 async function getApi(startIndex) {
-    try {
-        const response = await fetch(url);
-        const result = await response.json();
-        console.log(result);
-        const loader = document.querySelector('.loader');
-       
-        for (let i = 0; i < containers.length; i++) {
-            
-            
-            const container = containers[i];
-            const dataIndex = startIndex + i;
-
-            if (dataIndex < result.length) {
-                const item = result[dataIndex];
-                const images = item.images;
-                loader.style.display = "none";
-                
-                container.dataset.id = item.id;
-                const name = container.querySelector(".content-headline");
-                const text = container.querySelector(".content-text");
-                const imageElement = container.querySelector(".blog.imagecontainer img");
-                
-                name.textContent = item.name;
-                text.innerHTML = item.short_description;
-
-                if (images && images.length > 0) {
-                    imageElement.src = images[0].src; 
-                } else {
-                    
-                    imageElement.src = ""; 
-                }
-            } 
-            if (startIndex + containers.length >= result.length) {
-                
-                arrowRight.style.display = "none";
-            } else {
-                arrowRight.style.display = "grid"; 
-            }
-            console.log(startIndex);
-            if (startIndex >= 4) {
-                arrowLeft.style.display = "grid";
-            } else  {
-                arrowLeft.style.display = "none";
-            }
-        }
-    } catch (error) {
-        console.log("Ups!!!", error);
-    } finally {
-        console.log("Success!!!!");
+  try {
+    const response = await fetch(url);
+    const result = await response.json();
+    console.log(result);
+    const loader = document.querySelector(".loader");
+    
+    // Check if there is data to load
+    if (startIndex >= result.length) {
+      arrowRight.style.display = "none";
+      return;
+    } else {
+      arrowRight.style.display = "grid";
     }
-    containers.forEach((container) => {
-        const loader = container.querySelector('.loader');
-        loader.style.display = 'none';
-    });
+
+   
+    
+
+    // Clear existing containers
+    carouselContainer.innerHTML = "";
+
+    for (let i = startIndex; i < startIndex + 4 && i < result.length; i++) {
+      // Create a new blog container for each item
+      const container = createBlogContainer(result[i]);
+      
+      carouselContainer.appendChild(container);
+    }
+
+    currentIndex = startIndex;
+
+    if (startIndex >= 4) {
+      arrowLeft.style.display = "grid";
+    } else {
+      arrowLeft.style.display = "none";
+    }
+  } catch (error) {
+    console.log("Ups!!!", error);
+  } finally {
+    console.log("Success!!!!");
+  }
+}
+
+function createBlogContainer(item) {
+    
+  const blogContainer = document.createElement("div");
+  blogContainer.classList.add("blogcontainer");
+    carouselContainer.appendChild(blogContainer);
+
+  const loader = document.createElement("div");
+  loader.classList.add("loader");
+  blogContainer.appendChild(loader);
+  loader.style.display = "none";
+
+  const imageContainer = document.createElement("div");
+  imageContainer.classList.add("blog", "imagecontainer");
+  const image = document.createElement("img");
+  image.src = item.images[0]?.src || ""; 
+  image.alt = "Image";
+  imageContainer.appendChild(image);
+  blogContainer.appendChild(imageContainer);
+
+  const contentContainer = document.createElement("div");
+  contentContainer.classList.add("contentcontainer");
+  blogContainer.appendChild(contentContainer);
+
+  const contentHeadline = document.createElement("h2");
+  contentHeadline.classList.add("content-headline");
+  contentHeadline.textContent = item.name;
+  contentContainer.appendChild(contentHeadline);
+
+  const contentText = document.createElement("p");
+  contentText.classList.add("content-text");
+  contentText.innerHTML = item.short_description;
+  contentContainer.appendChild(contentText);
+
+  const link = document.createElement("a");
+  link.innerHTML = "Find out more";
+  contentContainer.appendChild(link);
+
+  
+
+  return blogContainer;
 }
 
 function loadNextResults() {
-    currentIndex += 4;
-    getApi(currentIndex);
+  currentIndex += 4;
+  getApi(currentIndex);
 }
+
 function loadPreviousResults() {
-    currentIndex -= 4;
-    getApi(currentIndex);
+  currentIndex -= 4;
+  if (currentIndex < 0) {
+    currentIndex = 0;
+  }
+  getApi(currentIndex);
 }
 
+arrowRight.addEventListener("click", loadNextResults);
+arrowLeft.addEventListener("click", loadPreviousResults);
 
-arrowRight.addEventListener('click', loadNextResults);
-arrowLeft.addEventListener('click', loadPreviousResults);
 getApi(currentIndex);
+
+const imageContainer = document.querySelector(".imageContainer");
+const images = imageContainer.querySelectorAll("img");
+let index = 0;
+const revealDelay = 5000; 
+
+function revealImage() {
+    if (index < images.length) {
+        const image = images[index];
+        images.forEach((img) => {
+            img.classList.remove("active");
+          });
+        // Make the current image the first child in the container
+        imageContainer.insertBefore(image, imageContainer.firstChild);
+        
+        // Add the 'active' class to reveal the image with a fade-in effect
+        image.classList.add("active");
+        
+        index++;
+    
+        // Call the next revealImage after a delay
+        setTimeout(revealImage, revealDelay);
+      } else {
+        // Reset the index to show images from the beginning
+        index = 0;
+        revealImage();
+      }
+    }
+    
+    // Call the function to start the animation
+    revealImage();
